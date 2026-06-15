e3fc316e7221c6196265b11639756f194a185f2d
Dun <dung8ahxh@gmail.com>
2026-06-15 11:00:41 +0700

    Initial Commit

---
+++ RealtimePoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealtimePoll
+//
+//  Created by MAC on 6/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RealtimePollTests/RealtimePollTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimePollTests.swift
+//  RealtimePollTests
+//
+//  Created by MAC on 6/15/26.
+//
+
+import Testing
+@testable import RealtimePoll
+
+struct RealtimePollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
